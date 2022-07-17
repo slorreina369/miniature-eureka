88d460fa8bf3107bf2bbc4a8b77c32110cd6f27f
Lorreina Guyett <slorreina369@gmail.com>
2022-07-16 17:43:28 -0700

    server.js created

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
