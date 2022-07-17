2e1e152d5c716441b8cdcc4e49b71db278d08a17
Lorreina Guyett <slorreina369@gmail.com>
2022-07-16 17:58:58 -0700

    connected the html pages to one another

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.static('public'));
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
